83a161b6df792f5461f4a623e8566f7ec14e21fd
Harsh Soni <harsh.here17@gmail.com>
2023-01-28 14:50:06 +0530

    constants add

---
+++ lib/src/ui/common/asset_files.dart
@@ -0,0 +1,8 @@
+class AssetFiles {
+  AssetFiles._();
+
+  // logos
+  static const String zappLogo = '⚡️';
+
+  // svgs
+}
---
+++ lib/src/ui/common/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  ColorConstants._();
+  static const backgroundLight = Color(0xFFFFFFFC);
+  static const backgroundDark = Color(0xFF000000);
+
+  static const primaryTextLight = Color(0xFF000000);
+  static const primaryTextDark = Color(0xFFFFFFFC);
+
+  static const secondaryText = Color(0xFFBEB7A4);
+
+  static const primarySwatch = Color(0xFFFF7F11);
+}
