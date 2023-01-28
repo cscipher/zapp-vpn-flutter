a4b9996ed0589a7f569c74cc1a6277458e587023
Harsh Soni <harsh.here17@gmail.com>
2023-01-28 16:47:30 +0530

    global platform variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:zapp_vpn/app.dart';
 
+final isIos = Platform.isIOS;
 void main() {
-  runApp(ZapVpnApp());
+  runApp(const ZapVpnApp());
 }
