d606cba3cb9bd31796573585a7c2119536f49700
Harsh Soni <harsh.here17@gmail.com>
2023-01-28 16:49:08 +0530

    splash screen implement

---
+++ lib/src/ui/pages/splash_page/splash_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:zapp_vpn/src/ui/common/zapp_color_constants.dart';
+import 'package:zapp_vpn/src/ui/common/zapp_string_constants.dart';
+import 'package:zapp_vpn/src/ui/common/zapp_text_styles.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      backgroundColor: theme.backgroundColor,
+      body: Center(
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.network(
+                    'https://picsum.photos/500',
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  'ZAPP VPN ${ZappStringConstants.thunder}',
+                  style: ZappFontStyles.bodyMediumL(color: theme.primaryColor),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 100.0),
+                  child: Text(
+                    'World\'s fastest VPN tunnel to hide your IP & surf safely!',
+                    textAlign: TextAlign.center,
+                    style: ZappFontStyles.bodyRegularXs(
+                      color: ZappColorConstants.secondaryTextLight,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                Text(
+                  '${ZappStringConstants.copyright} 2023 Cipher RJ',
+                  textAlign: TextAlign.center,
+                  style: ZappFontStyles.bodyRegularXs(
+                    color: ZappColorConstants.secondaryTextLight,
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 0,
+              left: 20,
+              right: 20,
+              child: Container(
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: theme.primaryColor,
+                  borderRadius:
+                      const BorderRadius.vertical(top: Radius.circular(20)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Easy Connection',
+                      style: ZappFontStyles.bodyMediumM(
+                        color: theme.backgroundColor,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      child: Text(
+                        'World\'s fastest VPN tunnel to hide your IP & surf safely!',
+                        textAlign: TextAlign.center,
+                        style: ZappFontStyles.bodyRegularXs(
+                          color: theme.backgroundColor,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 80),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
