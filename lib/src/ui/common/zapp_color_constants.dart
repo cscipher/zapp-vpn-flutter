75bd36aadc993b14ce1549ce437a5437b9c856ec
Harsh Soni <harsh.here17@gmail.com>
2023-01-29 17:19:16 +0530

    colors update

--- lib/src/ui/common/zapp_color_constants.dart
+++ lib/src/ui/common/zapp_color_constants.dart
@@ -8,9 +8,9 @@ class ZappColorConstants {
   static const primaryTextLight = Color(0xFF03080C);
   static const primaryTextDark = Color.fromARGB(255, 255, 255, 255);
 
-  static const secondaryTextLight = Color(0xFFBEB7A4);
+  static const secondaryTextLight = Color.fromARGB(255, 160, 154, 137);
   static const secondaryTextDark = Color.fromARGB(255, 232, 228, 220);
 
-  static const primaryColorLight = Color(0xFF306AA3);
-  static const primaryColorDark = Color(0XFF93b9df);
+  static const primaryColorLight = Color.fromARGB(255, 255, 115, 0);
+  static const primaryColorDark = Color.fromARGB(255, 255, 145, 54);
 }
