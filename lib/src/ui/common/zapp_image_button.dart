aaabeac7cc226eb1a3b31b2d2316880683565556
Harsh Soni <harsh.here17@gmail.com>
2023-01-29 13:17:44 +0530

    image button

---
+++ lib/src/ui/common/zapp_image_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ZappImageButton extends StatelessWidget {
+  final String path;
+  final double size;
+  final Function onPressed;
+  const ZappImageButton({
+    required this.path,
+    required this.onPressed,
+    this.size = 14,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () => onPressed.call(),
+        child: path.contains('.svg')
+            ? SvgPicture.asset(
+                path,
+                height: size,
+                width: size,
+              )
+            : Image.asset(
+                path,
+                height: size,
+                width: size,
+              ));
+  }
+}
