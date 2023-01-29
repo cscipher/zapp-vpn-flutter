ee8e38a53a41129d4e4fe651af6df3afdba987b6
Harsh Soni <harsh.here17@gmail.com>
2023-01-29 13:17:55 +0530

    text styles update

--- lib/src/ui/common/zapp_text_styles.dart
+++ lib/src/ui/common/zapp_text_styles.dart
@@ -4,45 +4,66 @@ import 'package:google_fonts/google_fonts.dart';
 class ZappFontStyles {
   ZappFontStyles._();
 
-  static TextStyle bodyMediumXs({Color? color}) => GoogleFonts.poppins(
+  static TextStyle bodyBoldXs({Color? color}) => GoogleFonts.notoSans(
         color: color,
         fontWeight: FontWeight.w700,
         fontSize: 12,
       );
-  static TextStyle bodyMediumS({Color? color}) => GoogleFonts.poppins(
+  static TextStyle bodyBoldS({Color? color}) => GoogleFonts.notoSans(
         color: color,
         fontWeight: FontWeight.w700,
-        fontSize: 14,
+        fontSize: 16,
       );
-  static TextStyle bodyMediumM({Color? color}) => GoogleFonts.poppins(
+  static TextStyle bodyBoldM({Color? color}) => GoogleFonts.notoSans(
         color: color,
         fontWeight: FontWeight.w700,
         fontSize: 20,
       );
-  static TextStyle bodyMediumL({Color? color}) => GoogleFonts.poppins(
+  static TextStyle bodyBoldL({Color? color}) => GoogleFonts.notoSans(
         color: color,
         fontWeight: FontWeight.w700,
         fontSize: 24,
       );
 
-  static TextStyle bodyRegularXs({Color? color}) => GoogleFonts.poppins(
+  static TextStyle bodyMediumXs({Color? color}) => GoogleFonts.notoSans(
         color: color,
-        fontWeight: FontWeight.normal,
+        fontWeight: FontWeight.w600,
         fontSize: 12,
       );
-  static TextStyle bodyRegularS({Color? color}) => GoogleFonts.poppins(
+  static TextStyle bodyMediumS({Color? color}) => GoogleFonts.notoSans(
         color: color,
-        fontWeight: FontWeight.normal,
-        fontSize: 14,
+        fontWeight: FontWeight.w600,
+        fontSize: 16,
       );
-  static TextStyle bodyRegularM({Color? color}) => GoogleFonts.poppins(
+  static TextStyle bodyMediumM({Color? color}) => GoogleFonts.notoSans(
         color: color,
-        fontWeight: FontWeight.normal,
+        fontWeight: FontWeight.w600,
         fontSize: 20,
       );
-  static TextStyle bodyRegularL({Color? color}) => GoogleFonts.poppins(
+  static TextStyle bodyMediumL({Color? color}) => GoogleFonts.notoSans(
         color: color,
-        fontWeight: FontWeight.normal,
+        fontWeight: FontWeight.w600,
+        fontSize: 24,
+      );
+
+  static TextStyle bodyRegularXs({Color? color}) => GoogleFonts.notoSans(
+        color: color,
+        fontWeight: FontWeight.w400,
+        fontSize: 12,
+      );
+  static TextStyle bodyRegularS({Color? color}) => GoogleFonts.notoSans(
+        color: color,
+        fontWeight: FontWeight.w400,
+        fontSize: 16,
+      );
+  static TextStyle bodyRegularM({Color? color}) => GoogleFonts.notoSans(
+        color: color,
+        fontWeight: FontWeight.w400,
+        fontSize: 20,
+      );
+  static TextStyle bodyRegularL({Color? color}) => GoogleFonts.notoSans(
+        color: color,
+        fontWeight: FontWeight.w400,
         fontSize: 24,
       );
 }
