62a8544dd80a3b7b543946477af7beae27641cb3
Harsh Soni <harsh.here17@gmail.com>
2023-01-29 13:16:42 +0530

    bloc DI add

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:zapp_vpn/src/config/theme_config.dart';
+import 'package:zapp_vpn/src/ui/pages/homepage/bloc/home_page_bloc.dart';
 import 'package:zapp_vpn/src/ui/pages/homepage/zapp_home_page.dart';
 
-class ZapVpnApp extends StatelessWidget {
-  const ZapVpnApp({Key? key}) : super(key: key);
+class ZappVpnApp extends StatelessWidget {
+  const ZappVpnApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         Provider<ThemeConfig>(create: (context) => ThemeConfig()),
+        BlocProvider<HomePageBloc>(create: (context) => HomePageBloc()),
       ],
       builder: (context, _) {
         final themeConfig = Provider.of<ThemeConfig>(context);
