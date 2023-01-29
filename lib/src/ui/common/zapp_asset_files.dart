683b81191ff6f734f05033e8bebbf64f7e4554fa
Harsh Soni <harsh.here17@gmail.com>
2023-01-29 17:18:34 +0530

    assets add

--- lib/src/ui/common/zapp_asset_files.dart
+++ lib/src/ui/common/zapp_asset_files.dart
@@ -5,7 +5,12 @@ class ZappAssetFiles {
   static const String zappLogo = '⚡️';
   static const String premiumCrown = 'assets/images/crown.svg';
   static const String premiumCrownV2 = 'assets/images/crownV2.png';
+  static const String backgroundImage = 'assets/images/background.jpg';
+  static const String thunder = 'assets/images/thunder.png';
 
   // lottie
-  static const String thumderLottie = 'assets/lottie/thunder.json';
+  static const String thunderLottie =
+      'https://lottie.host/cdf77ab9-ecc1-4a60-ad68-7594e8146c62/2jMN0JfHXy.json';
+  static const String bolt =
+      'https://lottie.host/138cfff8-1772-45be-b753-449f13c2ce08/0Y6rNpEdLr.json';
 }
