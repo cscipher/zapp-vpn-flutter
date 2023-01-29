9a1168ccfa152af9e4dbea7cd9d61b2c1a1d6440
Harsh Soni <harsh.here17@gmail.com>
2023-01-29 13:17:06 +0530

    error handling on global level

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zapp_vpn/app.dart';
 
 final isIos = Platform.isIOS;
 void main() {
-  runApp(const ZapVpnApp());
+  runZonedGuarded(
+    () => BlocOverrides.runZoned(
+      () async => runApp(const ZappVpnApp()),
+      blocObserver: Bloc.observer,
+    ),
+    (error, stackTrace) => log("===========Exception caught============",
+        error: error, stackTrace: stackTrace),
+  );
 }
