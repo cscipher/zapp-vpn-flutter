8f840220eb2062b541bedb590d5ee2dea1a40745
Harsh Soni <harsh.here17@gmail.com>
2023-01-29 17:19:31 +0530

    animated switcher on homepage

--- lib/src/ui/pages/homepage/zapp_home_page.dart
+++ lib/src/ui/pages/homepage/zapp_home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:zapp_vpn/src/ui/pages/homepage/bloc/home_page_bloc.dart';
 import 'package:zapp_vpn/src/ui/pages/homepage/widgets/zapp_home_page_widgets.dart';
+import 'package:zapp_vpn/src/ui/pages/splash_page/splash_page.dart';
 
 class ZappHomePage extends StatefulWidget {
   const ZappHomePage({super.key});
@@ -18,6 +19,47 @@ class _ZappHomePageState extends State<ZappHomePage> {
     super.initState();
   }
 
+  Widget _getPage(HomePageState state, ThemeData theme) {
+    if (state is HomePageLoadingState) {
+      return SplashPage(
+        index: state.dataIndex,
+      );
+    } else if (state is HomePageLoadedState) {
+      return SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              ZappHomePageHeaderWidget(
+                isDarkModeEnabled: state.isDarkModeEnabled,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 200,
+                child: const Placeholder(),
+              ),
+              const SizedBox(height: 50),
+              ZappHomePageConnectButtonWidget(
+                connectedSinceString: state.connectedSinceString,
+                vpnConnectionStatus: state.vpnConnectionStatus,
+                isDarkModeEnabled: state.isDarkModeEnabled,
+              ),
+              const Spacer(),
+              ZappHomePageServerSwitcherWidget(
+                isDarkModeEnabled: state.isDarkModeEnabled,
+                vpnConnectionStatus: state.vpnConnectionStatus,
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Container(
+        color: theme.primaryColor,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     if (bloc == null) {
@@ -28,46 +70,14 @@ class _ZappHomePageState extends State<ZappHomePage> {
 
     return Scaffold(
         backgroundColor: theme.backgroundColor,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: BlocBuilder<HomePageBloc, HomePageState>(
-              bloc: bloc,
-              builder: (context, state) {
-                switch (state.runtimeType) {
-                  case HomePageLoadedState:
-                    final loadedState = state as HomePageLoadedState;
-                    return Column(
-                      children: [
-                        ZappHomePageHeaderWidget(
-                          isDarkModeEnabled: loadedState.isDarkModeEnabled,
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          height: 200,
-                          child: const Placeholder(),
-                        ),
-                        const SizedBox(height: 50),
-                        ZappHomePageConnectButtonWidget(
-                          connectedSinceString:
-                              loadedState.connectedSinceString,
-                          isConnected: loadedState.isConnected,
-                          isDarkModeEnabled: loadedState.isDarkModeEnabled,
-                        ),
-                        const Spacer(),
-                        ZappHomePageServerSwitcherWidget(
-                          isDarkModeEnabled: loadedState.isDarkModeEnabled,
-                        ),
-                      ],
-                    );
-
-                  default:
-                    return const Center(
-                        child: CircularProgressIndicator.adaptive());
-                }
-              },
-            ),
-          ),
+        body: BlocBuilder<HomePageBloc, HomePageState>(
+          bloc: bloc,
+          builder: (context, state) {
+            return AnimatedSwitcher(
+              duration: const Duration(milliseconds: 350),
+              child: _getPage(state, theme),
+            );
+          },
         ));
   }
 }
