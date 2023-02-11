6e0bb73548cd307e57172665b7c18f612fc0ae25
Harsh Soni <harsh.here17@gmail.com>
2023-02-11 12:53:47 +0530

    main app run function change

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,11 @@
-import 'dart:async';
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zapp_vpn/app.dart';
+import 'package:zapp_vpn/src/config/zapp_vpn_config.dart';
 
 final isIos = Platform.isIOS;
 void main() {
-  runZonedGuarded(
-    () => BlocOverrides.runZoned(
-      () async => runApp(const ZappVpnApp()),
-      blocObserver: Bloc.observer,
-    ),
-    (error, stackTrace) => log("===========Exception caught============",
-        error: error, stackTrace: stackTrace),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ZappVpnApp());
 }
