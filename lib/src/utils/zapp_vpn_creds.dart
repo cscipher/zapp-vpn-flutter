22bebbff26395a3b0f3504c1d3ba71bfb1f4fe4d
Harsh Soni <harsh.here17@gmail.com>
2023-02-11 12:56:15 +0530

    vpn creds add

---
+++ lib/src/utils/zapp_vpn_creds.dart
@@ -0,0 +1,9 @@
+class ZappVpnCreds {
+  ZappVpnCreds._();
+
+  static const String groupIdentifier = 'group.com.cipher.zappVpn';
+  static const String providerBundleIdentifier = 'com.cipher.zappVpn.ZappVPN';
+  static const String localizedDescription = 'Zapp VPN';
+  static const String defaultUsername = 'freeopenvpn';
+  static const String defaultPassword = '078918550';
+}
