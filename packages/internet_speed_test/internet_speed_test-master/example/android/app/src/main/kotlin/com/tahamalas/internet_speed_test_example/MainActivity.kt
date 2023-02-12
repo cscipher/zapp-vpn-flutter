7bba30d392845ae02ad1265ef92b258c45ca7787
Harsh Soni <harsh.here17@gmail.com>
2023-02-12 17:47:39 +0530

    delete package

--- packages/internet_speed_test/internet_speed_test-master/example/android/app/src/main/kotlin/com/tahamalas/internet_speed_test_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tahamalas.internet_speed_test_example
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/internet_speed_test/internet_speed_test-master/ios/Classes/InternetSpeedTestPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface InternetSpeedTestPlugin : NSObject<FlutterPlugin>
-@end
